f5fa4857af13610568ccdf4b47a875db097eb011
Doug <nullvariable@gmail.com>
2014-04-17 15:00:28 -0400

    plugin headers

---
+++ raygun-for-wordpress.php
@@ -0,0 +1,12 @@
+<?php
+/*
+Plugin Name: Raygun for WordPress
+Version: 0.1-alpha
+Description: Raygun, for WordPress
+Author: Doug Cone
+Author URI: DougCone.com
+Plugin URI: https://github.com/nullvariable/raygun-for-wordpress
+Text Domain: raygun-for-wordpress
+Domain Path: /languages
+*/
+
